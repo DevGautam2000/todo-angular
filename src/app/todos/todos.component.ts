7f997b4ce11ef1ab87f5827cd4e76320e5b7b3d7
GAUTAM CHANDRA SAHA <devgautam1231@gmail.com>
2021-10-24 14:34:18 +0530

    fixed bugs

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -5,7 +5,7 @@
 
 */
 
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todos',
@@ -25,11 +25,45 @@ export class TodosComponent {
 
   selectedTodos: any = [];
 
+  warn(snackbar: any) {
+    snackbar.innerHTML = 'Enter a todo first';
+    snackbar.classList.add('warn');
+  }
+  active(snackbar: any) {
+    snackbar.innerHTML = 'Added to todos!';
+    snackbar.classList.add('active');
+  }
+
   add(element: any) {
-    !this.selectedTodos.includes(element)
+    !this.selectedTodos.includes(element) && element
       ? this.selectedTodos.push(element)
       : null;
+
+    const snackbar = <HTMLInputElement>document.querySelector('.snackbar');
+
+    !element ? this.warn(snackbar) : null;
+
+    !this.todoList.includes(element) && element ? this.active(snackbar) : null;
+
+    setTimeout(() => {
+      snackbar.classList.contains('active')
+        ? snackbar.classList.remove('active')
+        : null;
+    }, 1500);
+    setTimeout(() => {
+      snackbar.classList.contains('warn')
+        ? snackbar.classList.remove('warn')
+        : null;
+    }, 1500);
   }
+
+  getValue() {
+    const userInput = <HTMLInputElement>document.getElementById('user-input');
+
+    this.add(userInput.value);
+    userInput.value = '';
+  }
+
   check(index: any, element: any): void {
     this.inps = document.querySelectorAll('input');
     this.inps[index].checked = true;
@@ -39,7 +73,9 @@ export class TodosComponent {
   splice(elem: any, i: any) {
     this.inps = document.querySelectorAll('input');
     const index = this.todoList.indexOf(elem);
+    if (this.todoList.includes(elem)) {
+      this.inps[index].checked = false;
+    }
     this.selectedTodos.splice(i, 1);
-    this.inps[index].checked = false;
   }
 }
